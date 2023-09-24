ce0a893f83daf1e18c900b737800fd1af9605173
Willian_WB <wb.will.barbosa@gmail.com>
2023-09-24 20:28:43 -0300

    correção

--- src/br/com/ada/controller/PrincipalController.java
+++ src/br/com/ada/controller/PrincipalController.java
@@ -71,8 +71,6 @@ public class PrincipalController {
             curiosidadeController.createAndShowGUI();
         });
 
-		 
-
 		panel.add(golsButton);
 		panel.add(estatisticaButton);
 		panel.add(cartoesButton);
